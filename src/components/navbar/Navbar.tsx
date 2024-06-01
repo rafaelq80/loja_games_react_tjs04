c974d6cd1c8dd175cc451778cfbbc943846d80b2
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-06-01 11:20:52 -0300

    Update

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -28,19 +28,19 @@ function Navbar() {
                 </Link>
 
                 <div className="flex-1 flex justify-center items-center relative w-30 text-black">
-                        <form>
-                            <input className="h-9 w-90 rounded-lg px-4 py-4 focus:outline-none"
-                                type="search"
-                                placeholder="Pesquisar produto"
-                                id="busca"
-                                name="busca"
-                                required
-                            />
-                            <button type="submit" className="h-9 w-9 p-2.5 ms-2 text-sm font-medium text-white bg-teal-500 hover:bg-teal-900 rounded-lg border border-teal-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
-                                <MagnifyingGlass size={14} weight="bold"/>
-                            </button>
-                        </form>
-                    </div>
+                    <form className="w-3/4 flex justify-center">
+                        <input className="w-10/12 h-9 rounded-lg px-4 py-4 focus:outline-none"
+                            type="search"
+                            placeholder="Pesquisar produto"
+                            id="busca"
+                            name="busca"
+                            required
+                        />
+                        <button type="submit" className="h-9 w-9 p-2.5 ms-2 text-sm font-medium text-white bg-teal-500 hover:bg-teal-900 rounded-lg border border-teal-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
+                            <MagnifyingGlass size={14} weight="bold" />
+                        </button>
+                    </form>
+                </div>
 
                 <div className='flex gap-4 py-4'>
                     <Link to='/produtos' className='hover:underline'>Produtos</Link>
